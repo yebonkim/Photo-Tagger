a709ae09d462f17b52c600116949b18a7c5a6c22
Yebonkim <v954242@gmail.com>
2020-01-17 19:41:20 +0900

    Add searching duplicate image in ES at lambda

--- lambda/photo-tagger-add-es.py
+++ lambda/photo-tagger-add-es.py
@@ -1,6 +1,8 @@
 from botocore.vendored import requests
+import json
 
-host = 'Your ES Url'
+host = '<Your ES Address>'
+indexing_host = host + "/photo_tag/_doc"
 
 headers = {'Content-Type': 'application/json; charset=utf-8'}
 
@@ -9,5 +11,25 @@ def lambda_handler(event, context):
         "image_name": event["image_name"],
         "labels": event["labels"]
     }
-    res = requests.post(host, headers=headers, json=document)
-    print(res.text)
+    if check_duplicate(event["image_name"]) is False :
+        res = requests.post(indexing_host, headers=headers, json=document)
+        print(res.text)
+    
+def check_duplicate(image_name):
+    document = {
+        "query" : {
+            "match" : {
+                "image_name" : image_name
+            }
+        }
+    }
+    res = requests.get(host + "/photo_tag/_search/"+image_name, headers=headers, json=document)
+    res_json = json.loads(res.text)
+    res_count = 0
+    if "hits" in res_json and "hits" in res_json["hits"]:
+        res_count = len(res_json["hits"]["hits"])
+    
+    if res_count > 0:
+        return True
+    else:
+        return False
